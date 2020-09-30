fae028aa6788a71a03486f1d15c6725b8f3544e5
Alexander Voyteshonok <sasha1.www@gmail.com>
2020-09-30 11:08:59 +0300

    1 lab done

---
+++ Lab1/V6_1.sql
@@ -0,0 +1,32 @@
+﻿-- Создание базы данных
+CREATE DATABASE Alexander_Voyteshonok;
+GO
+
+-- Использование созданной БД в качестве рабочей
+USE Alexander_Voyteshonok;
+GO
+
+-- Создание в текущей БД схем sales и persons
+CREATE SCHEMA sales;
+GO
+
+CREATE SCHEMA persons;
+GO
+
+-- Создание таблицы Orders в схеме sales с полем OrderNum типа Int 
+CREATE TABLE sales.Orders(OrderNum INT NULL);
+GO
+
+-- Создание резервной копии всей базы данных на диске
+BACKUP DATABASE Alexander_Voyteshonok
+	TO DISK = N'D:\DBlabs\Lab1\Alexander_Voyteshonok.bak';
+GO
+
+-- Удаление базы данных
+DROP DATABASE Alexander_Voyteshonok;
+GO
+
+-- Восстановление базы данных из резервной
+RESTORE DATABASE Alexander_Voyteshonok
+	FROM DISK = N'D:\DBlabs\Lab1\Alexander_Voyteshonok.bak';
+GO
---
+++ Lab1/V6_2.sql
@@ -0,0 +1,19 @@
+﻿USE AdventureWorks2012;
+GO
+
+--Вывести на экран список отделов, названия которых начинаются на букву ‘F’ и заканчиваются на букву ‘е’.
+SELECT DepartmentID, Name FROM HumanResources.Department WHERE Name LIKE 'F%e';
+GO
+
+/*Вывести на экран среднее количество часов отпуска и среднее количество больничных часов у сотрудников.
+Назовите столбцы с результатами ‘AvgVacationHours’ и ‘AvgSickLeaveHours’ для отпусков и больничных соответственно.*/
+SELECT AVG(Employee.VacationHours) as AvgVacationHours, AVG(Employee.SickLeaveHours) as AvgSickLeaveHours
+FROM HumanResources.Employee;
+GO
+
+/*Вывести на экран сотрудников, которым больше 65-ти лет на настоящий момент.
+Вывести также количество лет, прошедших с момента трудоустройства, в столбце с именем ‘YearsWorked’.*/
+SELECT Employee.BusinessEntityID, Employee.JobTitle, Employee.Gender, DATEDIFF(YEAR, Employee.HireDate, CURRENT_TIMESTAMP) 'YearsWorked'
+FROM HumanResources.Employee
+WHERE DATEDIFF(YEAR, Employee.BirthDate, CURRENT_TIMESTAMP) > 65;
+GO
